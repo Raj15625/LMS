73a85bd7988fe0e3af0b19a5d01c2591db4a1af9
Raj15625 <gauravprasad546@gmail.com>
2025-07-29 17:54:41 +0530

    initial

---
+++ src/pages/student/CoursesList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CoursesList = () => {
+  return (
+    <div className="p-6 text-2xl font-bold text-blue-600">
+      Hello! I am the Courses List page.
+    </div>
+  );
+};
+
+export default CoursesList;
